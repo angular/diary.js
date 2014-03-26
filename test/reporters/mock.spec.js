63ba0c8a21f8258703116070f81b091d00edaeff
Merrick Christensen <merrick.christensen@gmail.com>
2014-03-26 16:47:45 -0600

    feature(MockReporter): simplify tests and make them less silly

--- test/reporters/mock.spec.js
+++ test/reporters/mock.spec.js
@@ -1,32 +1,36 @@
 import {MockReporter} from '../../src/reporters/mock';
 
 describe('MockReporter', () => {
-  var reporter;
-
-  beforeEach(() => {
-    reporter = new MockReporter();
+  it('should create an object with a log property set to an empty array', () => {
+    var reporter = new MockReporter();
+    expect(reporter.logs).toEqual([]);
   });
 
-  describe('constructor', () => {
-
-    it('should construct a class', () => {
-      expect(reporter).toBeDefined();
+  it('should take a info log object and store it in the log array as a string', () => {
+    var reporter = new MockReporter();
+    reporter.receive({
+      level: 'info',
+      message: 'an info message',
+      group: 'test'
     });
-
+    expect(reporter.logs).toEqual(['[info|test] an info message']);
   });
 
-  describe('receive', () => {
+  it('should log messages into the log property in the proper order', () => {
+    var reporter = new MockReporter();
 
-    it('should invoke console.info at the info level', () => {
-      expect(reporter.logs.length).toEqual(0);
-
-      reporter.receive({
-        level: 'info',
-        message: 'an info message',
-        group: 'test'
-      });
-
-      expect(reporter.logs[0]).toEqual('[info|test] an info message');
+    reporter.receive({
+      level: 'info',
+      message: 'an info message',
+      group: 'test'
     });
+
+    reporter.receive({
+      level: 'error',
+      message: 'an error message',
+      group: 'test'
+    });
+
+    expect(reporter.logs).toEqual(['[info|test] an info message', '[error|test] an error message']);
   });
 });
